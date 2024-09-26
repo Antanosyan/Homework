805edebf9799e31daf13658c3c3e194e6680e7d5
Antanosyan <antanosyan1987@gmail.com>
2024-09-26 11:38:22 +0400

    modifying task

--- src/functionOverloading/CalculateIncome.java
+++ src/functionOverloading/CalculateIncome.java
@@ -3,22 +3,23 @@ package functionOverloading;
 public class CalculateIncome {
     public static void main(String[] args) {
         System.out.println(calculateIncomeTax(2000));
-        System.out.println(calculateIncomeTax(2000, 0.30));
+        System.out.println(calculateIncomeTax(2000, 30));
         System.out.println(calculateIncomeTax(2000, true));
-
     }
-
-    static double calculateIncomeTax(double salary) {
-        double defaultTaxRate = 0.20;
-        return salary * defaultTaxRate;
+    // income tax with a custom tax rate
+    static double calculateIncomeTax(double grossSalary, double customTaxRate) {
+        return grossSalary * customTaxRate / 100;
     }
-
-    static double calculateIncomeTax(double salary, double customTaxRate) {
-        return salary * customTaxRate;
+    // income tax with default tax rate 20%
+    static double calculateIncomeTax(double grossSalary) {
+        return calculateIncomeTax(grossSalary, 20.0);
     }
-
-    static double calculateIncomeTax(double salary, boolean isIT) {
-        double taxRate = isIT ? 0.10 : 0.20;
-        return salary * taxRate;
+    // 10% for IT, default 20%
+    public static double calculateIncomeTax(double grossSalary, boolean isITEmployee) {
+        if (isITEmployee) {
+            return calculateIncomeTax(grossSalary, 10);  // Reusing the second method for IT employees (10%)
+        } else {
+            return calculateIncomeTax(grossSalary);  // Reusing the first method (default 20%)
+        }
     }
 }
