7b5858cf3e444f58b89d618ae931120e5f32307d
Antanosyan <antanosyan1987@gmail.com>
2024-12-08 11:34:42 +0400

    modifying  task

--- src/homework_07_12/ConvertFromDecimalToBinary.java
+++ src/homework_07_12/ConvertFromDecimalToBinary.java
@@ -8,4 +8,12 @@ public class ConvertFromDecimalToBinary {
         }
         return fromDecimalToBinary(number / 2) + number % 2;
     }
+
+
+    public static String toBinary(int number, int sizeOfBits) {
+        if (sizeOfBits == 0) {
+            return "";
+        }
+        return toBinary(number >> 1, sizeOfBits - 1) + ((number & 1) == 0 ? "0" : "1");
+    }
 }
--- src/homework_07_12/Test.java
+++ src/homework_07_12/Test.java
@@ -3,8 +3,13 @@ package homework_07_12;
 public class Test {
     public static void main(String[] args) {
         int[] array = {75, 47, 98, 124, 2, 87};
+        int[] arrayIncludingNegativeValues = {-75, 47, -98, 124, -2, 87};
         for (int number : array) {
             System.out.println(ConvertFromDecimalToBinary.fromDecimalToBinary(number));
+        }
+        System.out.println("-------------------------");
+        for (int number : arrayIncludingNegativeValues) {
+            System.out.println(ConvertFromDecimalToBinary.toBinary(number,8));
         }
     }
 }
