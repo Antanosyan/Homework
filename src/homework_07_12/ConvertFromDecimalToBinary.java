d9ad265e4a619960fa03f4590eaab96c98575be5
Antanosyan <antanosyan1987@gmail.com>
2024-12-08 17:45:47 +0400

    some changes

--- src/homework_07_12/ConvertFromDecimalToBinary.java
+++ src/homework_07_12/ConvertFromDecimalToBinary.java
@@ -3,17 +3,16 @@ package homework_07_12;
 public class ConvertFromDecimalToBinary {
 
     public static String fromDecimalToBinary(int number) {
-        if (number == 0) {
-            return "";
+        try {
+            if (number < 0) {
+                throw new NegativeNumberException();
+            }
+            if (number == 0) {
+                return "";
+            }
+            return fromDecimalToBinary(number >> 1) + ((number & 1) == 0 ? "0" : "1");
+        } catch (NegativeNumberException e) {
+            return e.getMessage();
         }
-        return fromDecimalToBinary(number / 2) + number % 2;
-    }
-
-
-    public static String toBinary(int number, int sizeOfBits) {
-        if (sizeOfBits == 0) {
-            return "";
-        }
-        return toBinary(number >> 1, sizeOfBits - 1) + ((number & 1) == 0 ? "0" : "1");
     }
 }
